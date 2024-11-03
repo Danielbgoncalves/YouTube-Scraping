851b18fbe2e3aa0b7ed705bcdb0f17d6250e6531
Daniel Borges <44danbor@gmail.com>
2024-11-03 00:00:39 -0300

    Nem era pra essas coisas terem ido, elas estão no gitignore

--- bosta.py
+++
@@ -1,8 +0,0 @@
-link = input("Cole o link do YouTube\n=>")
-print(f"Voce digitou: \'{link}\'")
-
-inicio = link.find("list=")
-print(f"A parte a retirar é {inicio}")
-inicio +=5
-final = link[inicio:inicio + 37]
-print(f"A parte final é {final}")
